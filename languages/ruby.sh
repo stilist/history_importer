1abf4554cae6d06347ef88b87837477def880fcd
Jordan Cole <stilist@ratafia.info>
2016-12-24 15:07:42 -0800

    Fix bug with Ruby script

--- languages/ruby.sh
+++ languages/ruby.sh
@@ -8,11 +8,11 @@ if [ "$?" -ne "0" ] ; then
   gem install bundler --no-document --user-install
 fi
 
-bundle check || bundle install
-
 # Extract the Ruby version (e.g. `2.0.0`).
 ruby_version="$(ruby -v | sed -E 's/^.+(([0-9]+\.?){3})p.+$/\1/')"
 bundler_path="$HOME/.gem/ruby/$ruby_version/bin"
+
+"$bundler_path/bundle" check || "$bundler_path/bundle" install --jobs 4
 
 for file in ../importers/*.rb ; do
   [ -r "$file" ] && [ -f "$file" ] && "$bundler_path/bundle" exec ruby "$file"
