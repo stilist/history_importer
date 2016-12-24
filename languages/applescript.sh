4933831ab92aaaf07ab5ec1dcd2f6f2495b2ef23
Jordan Cole <stilist@ratafia.info>
2016-12-24 15:08:22 -0800

    Fix bug with AppleScript

--- languages/applescript.sh
+++ languages/applescript.sh
@@ -6,7 +6,7 @@ IFS=$'\n\t'
 which osascript
 if [ "$?" -eq "0" ] ; then
   for file in ../importers/*.sh ; do
-    [ -r "$file" ] && [ -f "$file" ] && osascript -l jscr -s o "$file"
+    [ -r "$file" ] && [ -f "$file" ] && osascript -l JavaScript -s o "$file"
   done
   unset file
 fi
