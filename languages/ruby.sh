c584a6921c85a6cee5966f10d2220c2f496a4963
Jordan Cole <stilist@ratafia.info>
2016-12-30 07:50:55 -0800

    Fix bundler installation

--- languages/ruby.sh
+++ languages/ruby.sh
@@ -3,10 +3,7 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-gem list -i bundler &>/dev/null
-if [ "$?" -ne "0" ] ; then
-  gem install bundler --no-document --user-install
-fi
+gem install bundler --no-document --user-install --conservative --minimal-deps --verbose
 
 # Extract the Ruby version (e.g. `2.0.0`).
 ruby_version="$(ruby -v | sed -E 's/^.+(([0-9]+\.?){3})p.+$/\1/')"
