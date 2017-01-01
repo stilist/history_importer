d37700b041566b5d2d83a6c5f80895469f2f849d
Jordan Cole <stilist@ratafia.info>
2017-01-01 12:32:56 -0800

    Fix Bundler gem install path

--- languages/ruby.sh
+++ languages/ruby.sh
@@ -9,7 +9,7 @@ gem install bundler --no-document --user-install --conservative --minimal-deps -
 ruby_version="$(ruby -v | sed -E 's/^.+(([0-9]+\.?){3})p.+$/\1/')"
 bundler_path="$HOME/.gem/ruby/$ruby_version/bin"
 
-"$bundler_path/bundle" check || "$bundler_path/bundle" install --jobs 4
+"$bundler_path/bundle" check || "$bundler_path/bundle" install --jobs 4 --path vendor/bundle
 
 for file in ../importers/*.rb ; do
   [ -r "$file" ] && [ -f "$file" ] && "$bundler_path/bundle" exec ruby "$file"
