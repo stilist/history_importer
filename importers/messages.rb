4e550b8439c0ad20c5495215f8390e0cad821896
Jordan Cole <stilist@ratafia.info>
2017-01-02 11:24:32 -0800

    Import messages archives

--- importers/messages.rb
+++ importers/messages.rb
@@ -6,6 +6,11 @@ enforce_os('Darwin')
 import_file('~/Library/Messages/chat.db', 'messages')
 
 source = File.expand_path('~/Library/Messages')
-destination = "#{ENV.fetch("HISTORY_DATA_PATH")}/data/messages"
-FileUtils.mkdir_p(destination)
-FileUtils.cp_r("#{source}/attachments", destination, preserve: true, verbose: true)
+directories = %w(Archive Attachments)
+directories.each do |directory|
+  destination = "#{ENV.fetch("HISTORY_DATA_PATH")}/data/messages/#{directory}"
+  FileUtils.mkdir_p(destination)
+  FileUtils.cp_r("#{source}/#{directory.downcase}", destination,
+                 preserve: true,
+                 verbose: true)
+end
