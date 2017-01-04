43080111e2a25ed31c5384b7cb6dd39755430ec3
Jordan Cole <stilist@ratafia.info>
2017-01-03 18:51:06 -0800

    Document destination_filename param

--- support/import_file.rb
+++ support/import_file.rb
@@ -8,6 +8,9 @@ require_relative 'hardware_model'
 # @param path [String] path to locate the source file
 # @param data_source [String] the app (or other source) the data is from -- for
 #   example, +'safari'+
+# @param destination_filename [String, nil] custom filename for the imported
+#   file; useful if the source filename doesn't convey the data type -- for
+#   example, iOS backups use SHA-1 to generate filenames
 def import_file(path, data_source, destination_filename: nil)
   raise 'Source not passed' if !path
   raise 'Need a source type (e.g. "safari")' if !data_source
