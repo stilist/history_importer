1cd79f563833c248690b9c8112dd61f7b52defbf
Jordan Cole <stilist@ratafia.info>
2017-01-03 19:58:35 -0800

    Fix duplicated Messages import paths

--- importers/messages.rb
+++ importers/messages.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'fileutils'
 require_relative '../support/enforce_os'
 require_relative '../support/import_file'
@@ -6,11 +8,11 @@ enforce_os('Darwin')
 import_file('~/Library/Messages/chat.db', 'messages')
 
 source = File.expand_path('~/Library/Messages')
+destination = File.expand_path("#{ENV.fetch("HISTORY_DATA_PATH")}/data/messages/")
 directories = %w(Archive Attachments)
 directories.each do |directory|
-  destination = "#{ENV.fetch("HISTORY_DATA_PATH")}/data/messages/#{directory}"
-  FileUtils.mkdir_p(destination)
-  FileUtils.cp_r("#{source}/#{directory.downcase}", destination,
+  FileUtils.mkdir_p("#{destination}/#{directory.downcase}")
+  FileUtils.cp_r("#{source}/#{directory}/.", "#{destination}/#{directory.downcase}",
                  preserve: true,
                  verbose: true)
 end
